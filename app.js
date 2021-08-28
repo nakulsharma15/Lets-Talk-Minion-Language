2c644c3f48949cbad1dedb86e922e44fdd1d7bbe
nakulsharma15 <nakuls309@gmail.com>
2021-08-28 20:30:03 +0530

    Update index.html

---
+++ app.js
@@ -0,0 +1,9 @@
+var btnTranslate = document.querySelector("#btn-translate");
+
+function clickEventHandler(){
+    console.log("Clicked")
+}
+
+btnTranslate.addEventListener("click",clickEventHandler);
+
+
