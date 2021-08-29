c423b6fb89abd60523cc8332f4f8874714e043cf
nakulsharma15 <nakuls309@gmail.com>
2021-08-29 12:34:58 +0530

    Added Translation functionality

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
+var serverURL = "https://api.funtranslations.com/translate/minion.json";
+
+function getTranslationURL(text) {
+   return serverURL + "?text=" + text;
+}
 
 function clickEventHandler(){
-   outputDiv.innerText = "Translated: " + txtInput.value;
+  
+  var inputTxt = txtInput.value;
+
+  fetch(getTranslationURL(inputTxt))
+  .then(response => response.json())
+  .then(json => {
+     var translatedText = json.contents.translated;
+     outputDiv.innerText = translatedText;
+  })
+
+
 };
 
 btnTranslate.addEventListener("click",clickEventHandler);
